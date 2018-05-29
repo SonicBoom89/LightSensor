039ea8040cbf64298864e264e86635de5f24a2d7
bergb <bergbauer.wolfgang@googlemail.com>
2018-05-29 16:42:20 +0200

    TemperatureSensor added

--- Rest_API.py
+++ Rest_API.py
@@ -2,23 +2,34 @@ from flask import Flask, abort, jsonify, request
 
 from Logger import Logger
 from LightSensorController import LightSensorController
+from TemperatureSensorController import TemperatureSensorController
 from time import localtime, strftime
+
 
 app = Flask(__name__)
 
 _log = Logger()
 _lightSensorController = LightSensorController()
+_temperatureSensorController = TemperatureSensorController()
 
 def startWebApi():
     app.run(debug=True, host="0.0.0.0", threaded=True)
 
 
 @app.route('/lightsensor')
-def poolpump():
+def lightsensor():
     _log.info("GET Request received: " + str(request))
     lightLevel = _lightSensorController.getLightLevel()
     _log.info("LightSensor Level Status [" + str(lightLevel) + "]")
     return jsonify(Device="LightSensor", LightLevel=lightLevel,
+                   Timestamp=strftime("%a, %d %b %Y %H:%M:%S +0000", localtime()))
+
+@app.route('/temperature')
+def temperature():
+    _log.info("GET Request received: " + str(request))
+    temperature = _temperatureSensorController.getTemperature()
+    _log.info("Temperature [" + str(temperature) + "]")
+    return jsonify(Device="Temperature Sensor", Temperature=temperature,
                    Timestamp=strftime("%a, %d %b %Y %H:%M:%S +0000", localtime()))
 
 
---
+++ TemperatureSensorController.py
@@ -0,0 +1,18 @@
+
+
+class TemperatureSensorController(object):
+
+    def getTemperature(self):
+
+        # 1-wire Slave Datei lesen
+        file = open('/sys/bus/w1/devices/28-000005d2e508/w1_slave')
+        filecontent = file.read()
+        file.close()
+
+        # Temperaturwerte auslesen und konvertieren
+        stringvalue = filecontent.split("\n")[1].split(" ")[9]
+        temperature = float(stringvalue[2:]) / 1000
+
+        # Temperatur ausgeben
+        rueckgabewert = '%6.2f' % temperature
+        return (rueckgabewert)
